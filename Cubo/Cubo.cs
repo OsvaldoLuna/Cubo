2aa2cdf3eaf0e26cf5829d46072a73f7984b7803
Osvaldo Luna <osvaldounam18@gmail.com>
2017-11-16 19:30:04 -0600

    Programa que pide y muestra el volumen de un cubo

---
+++ Cubo/Cubo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cual es la medida de un lado del cubo: ");
+            int cubo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dimensiones: " + cubo + " alto, " + cubo + " ancho, " + cubo + " profundidad ");
+            Console.WriteLine("Volumen. " + cubo * cubo * cubo);
+            Console.ReadKey();
+        }
+    }
+}
